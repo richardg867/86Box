0576c0b1812a908a23da070bdbf38656b24519b7
richardg867 <richardg867@gmail.com>
2018-07-24 22:16:01 -0300

    Update codegen_x86.h

--- src/cpu/codegen_x86.h
+++ src/cpu/codegen_x86.h
@@ -2,10 +2,10 @@
 #define BLOCK_MASK 0x3fff
 #define BLOCK_START 0
 
-#define HASH_SIZE 0x20000
-#define HASH_MASK 0x1ffff
+#define HASH_SIZE 0x2000000
+#define HASH_MASK 0x1ffffff
 
-#define HASH(l) ((l) & 0x1ffff)
+#define HASH(l) ((l) & 0x1ffffff)
 
 #define BLOCK_EXIT_OFFSET 0x7f0
 #define BLOCK_GPF_OFFSET (BLOCK_EXIT_OFFSET - 20)
