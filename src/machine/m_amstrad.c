b6e73278ab5312d9fabe160bab18584c2e54c43d
RichardG867 <richardg867@gmail.com>
2021-05-20 22:51:55 -0300

    Game port overhaul

--- src/machine/m_amstrad.c
+++ src/machine/m_amstrad.c
@@ -2510,8 +2510,7 @@ machine_amstrad_init(const machine_t *model, int type)
 	mouse_set_poll(ms_poll, ams);
     }
 
-    if (joystick_type)
-	device_add(&gameport_device);
+    standalone_gameport_type = &gameport_device;
 }
 
 
